4aeb8576a26016185ba7985c38d373d3f462f2c4
pavelegorenkov <epipasha@gmail.com>
2023-04-16 17:33:55 +0300

    Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,16 @@ import 'package:menu_planner/src/di.dart';
 import 'src/app.dart';
 import 'src/ui/app_bloc_observer.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   setup();
 
